9e0e0c4e47d6e28c41dac1297ad8f121ac6fe5b2
Abhishek Pal <30362786+Abhishek40@users.noreply.github.com>
2024-02-26 13:06:04 +0530

    Update pipeline-stack.ts

--- app-test/lib/pipeline-stack.ts
+++ app-test/lib/pipeline-stack.ts
@@ -1,14 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
+import {AppPipelineStage} from './pipeline-stage';
 
 export class AppTestPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
         
         // Creates a CodeCommit repository called 'WorkshopRepo'
-        new codecommit.Repository(this, 'AppTestRepo', {
+        const repo = new codecommit.Repository(this, 'AppTestRepo', {
             repositoryName: "AppTestRepo"
         });
+        
+        // Pipeline declaration
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'AppTestPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                    input: CodePipelineSource.codeCommit(repo, 'master'),
+                    installCommands: [
+                        'npm install -g aws-cdk'
+                    ],
+                    commands: [
+                        'npm ci',
+                        'npm run build',
+                        'npx cdk synth'
+                    ]
+                }
+            )
+        });
+        
+        const deploy = new AppPipelineStage(this, 'Deploy');
+        const deployStage = pipeline.addStage(deploy);
     }
 }
