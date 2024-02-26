a20d8adb84da82a3e350dcf1155cf4de3bb8020c
Abhishek Pal <30362786+Abhishek40@users.noreply.github.com>
2024-02-26 13:08:03 +0530

    Update app-test-stack.ts

--- app-test/lib/app-test-stack.ts
+++ app-test/lib/app-test-stack.ts
@@ -16,15 +16,20 @@
 
 //     topic.addSubscription(new subs.SqsSubscription(queue));
 //   }
-// }
+//// }
 
 import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib/core';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Cluster, ContainerImage, TaskDefinition, Compatibility, FargateService } from 'aws-cdk-lib/aws-ecs';
+import * as ecspatterns from 'aws-cdk-lib/aws-ecs-patterns';
+//import * as logs from 'aws-cdk-lib/aws-logs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 export class AppTestStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
     //Create VPC
@@ -33,23 +38,32 @@ export class AppTestStack extends cdk.Stack {
     //Create ECS Cluster
     const cluster = new Cluster(this, 'PetClinicCluster', { vpc });
     
-    //Define task definition
-    const taskDefinition = new TaskDefinition(this, 'petClinicTask', {
-        compatibility: Compatibility.FARGATE,
-        cpu: '256',
-        memoryMiB: '512',
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "AppTestFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("ikolaxis/petclinic") },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is true
     });
+    // //Define task definition
+    // const taskDefinition = new TaskDefinition(this, 'petClinicTask', {
+    //     compatibility: Compatibility.FARGATE,
+    //     cpu: '256',
+    //     memoryMiB: '512',
+    // });
     
-    //Add container to task definition
-    const container = taskDefinition.addContainer('petClinicContainer', {
-        image: ContainerImage.fromRegistry('ikolaxis/petclinic'),
-        memoryReservationMiB: 256,
-    });
+    // //Add container to task definition
+    // const container = taskDefinition.addContainer('petClinicContainer', {
+    //     image: ContainerImage.fromRegistry('ikolaxis/petclinic'),
+    //     memoryReservationMiB: 256,
+    // });
     
-    //Create ECS service
-    new FargateService(this, 'PetClinicFargateService', {
-        cluster,
-        taskDefinition,
-    });
+    // //Create ECS service
+    // new FargateService(this, 'PetClinicFargateService', {
+    //     cluster,
+    //     taskDefinition,
+    // });
+    
   }
 }
